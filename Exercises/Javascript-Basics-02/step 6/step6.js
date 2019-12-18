e30a74966bb19436ca6f6107e0765abe6dca48c9
abd elmajeed <abdelmajeedjurdi6@gmail.com>
2019-12-18 16:35:08 +0200

    step6.js completed

---
+++ Exercises/Javascript-Basics-02/step 6/step6.js
@@ -0,0 +1,12 @@
+var imageLink;
+var x = document.querySelectorAll("img");
+for (let i = 0; i < x.length; i++) {
+  x[i].addEventListener("mouseover", function() {
+    x[i].src = "images/image" + (i + 1) + "_2.jpg";
+  });
+  x[i].addEventListener("mouseout", function() {
+    imageLink = x[i].src;
+    imageLink = imageLink.replace("_2", "");
+    x[i].src = imageLink;
+  });
+}
