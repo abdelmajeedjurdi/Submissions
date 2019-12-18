c35486b46ec5e6d8dd83f088ac7561649baf0e0d
abd elmajeed <abdelmajeedjurdi6@gmail.com>
2019-12-18 16:23:18 +0200

    step5.js completed

---
+++ Exercises/Javascript-Basics-02/step 5/step5.js
@@ -0,0 +1,6 @@
+var x = document.querySelectorAll("img");
+for (let i = 0; i < x.length; i++) {
+  x[i].addEventListener("mouseover", function() {
+    x[i].src = "images/image" + (i + 1) + "_2.jpg";
+  });
+}
