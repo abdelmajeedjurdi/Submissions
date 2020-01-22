56ddd58acd80ea3a3bde2886a7cea8a462ac6aab
abd elmajeed <abdelmajeedjurdi6@gmail.com>
2020-01-22 13:39:40 +0200

    step2

---
+++ Challenge/movie-database/index.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const app = express();
+const PORT = 8080;
+app.get('/', (req, res) => {
+  res.send('ok');
+});
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
