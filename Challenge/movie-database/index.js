78d2ad97c010a5e51004da93eeca78ba83785eab
abd elmajeed <abdelmajeedjurdi6@gmail.com>
2020-01-23 10:36:47 +0200

    step4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -3,6 +3,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('ok');
 });
+
 app.get('/test', (req, res) => {
     res.json('status:200, message:"ok"');
   });
@@ -12,3 +13,20 @@ app.get('/time', (req, res) => {
   res.json({status:200, message:  new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds()})
 });
 
+app.get('/hello/:ID', (req, res) => {
+  res.json({status:200, message:"Hello " + req.params.ID});
+});
+
+app.get('/search', (req, res) => 
+  {
+    if(req.query.b) res.json({ status:200, data: req.query.b});
+      else
+        {
+          res.json(
+            {
+              status: 500,
+              error: true,
+              message: "you have to provide a search"
+            });
+        }
+  });
