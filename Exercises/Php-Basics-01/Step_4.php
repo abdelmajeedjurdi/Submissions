1be8a7d806820780463f8913efdfa860f2ae1058
abdelmajeedjurdi <abdelmajeedjurdi6@gmail.com>
2020-03-18 00:19:14 +0200

    step-4

---
+++ Exercises/Php-Basics-01/Step_4.php
@@ -0,0 +1,9 @@
+<?php
+$name = " Abd";
+$position = " I am tech student" ;
+$github_url = " , please check my github link : https://github.com/abdelmajeedjurdi";
+
+ echo "my name is" .$name; 
+ echo " , and  $position ";
+echo  $github_url;
+?>
