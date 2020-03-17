ea3742df52c3bf65e818ce2e579d4f6d29dc4274
abdelmajeedjurdi <abdelmajeedjurdi6@gmail.com>
2020-03-18 01:12:01 +0200

    step-5

---
+++ Exercises/Php-Basics-01/Step_5.php
@@ -0,0 +1,11 @@
+<?php
+$course_name = "php ";
+$enrolled_students = 12;
+$price = "30$ USD ";
+$on_discount = "Yes / No";
+
+echo " a course title that I like to learn is " .$course_name ;
+echo "\n  the number of enrolled students : $enrolled_students";
+echo "\n the course price is  $price";
+echo "\n if the course is on discount " .$on_discount; 
+?>
