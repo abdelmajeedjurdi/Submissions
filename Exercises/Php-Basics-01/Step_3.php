fcac651920994bf220395ba0594d0b04ba14997b
abdelmajeedjurdi <abdelmajeedjurdi6@gmail.com>
2020-03-17 23:51:46 +0200

    step-3

---
+++ Exercises/Php-Basics-01/Step_3.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Abd";
+$email ="abdelmajeed1@gmail.com";
+echo "my name is " .$name ;
+echo "\n and my email is " .$email;
+echo "\n";
+$height = 33;
+$width =21;
+$area = $height * $width ;
+echo "the area is " .$area;
+?>
