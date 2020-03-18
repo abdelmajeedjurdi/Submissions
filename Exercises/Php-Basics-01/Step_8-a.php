9783765451e95ce8a21d63fe4f4f41c04d53bba5
abdelmajeedjurdi <abdelmajeedjurdi6@gmail.com>
2020-03-18 02:16:33 +0200

    step-8-a

---
+++ Exercises/Php-Basics-01/Step_8-a.php
@@ -0,0 +1,3 @@
+<?php
+$my_array =[ "Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+echo "Hello Sir, do you have " . $my_array[0] . ", " . $my_array[1] . " , " . $my_array[2] . "?  Also if you sell fruits can I find a" . $my_array[5] . "." ;
