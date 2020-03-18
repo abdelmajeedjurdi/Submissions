d83751068f83ba7e0d7cddc4947544665246db1f
abdelmajeedjurdi <abdelmajeedjurdi6@gmail.com>
2020-03-18 13:29:00 +0200

    Step_8-b

--- Exercises/Php-Basics-01/Step_8-b.php
+++ Exercises/Php-Basics-01/Step_8-b.php
@@ -1,8 +1,10 @@
 <?php
-$my_array = eat [
-egss [" balade", "mazere3"] ,
-milk ["Fresh", "Taanayel"],
-water-pack ["Tanoureen", "Reem"]
-];
-  echo "Hey Sir, Please I need 1 pack of" .$my_array [0] [0] ;
+$my_array = array (  
+"egss ('balade', 'mazere3')",
+"milk ('Fresh', 'Taanayel')",
+"water-pack ('Tanoureen', 'Reem')"
+
+);
+  echo "Hey Sir, Please I need 1 pack of balade ".$my_array['eggs'][] = 'eggs';
+  echo "\n and 3 Reem " .$my_array['water-pack'][] = 'water-pack';
 ?>
